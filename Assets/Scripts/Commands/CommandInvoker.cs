24793acd5d3d10317433f32ce0973bb51adf559c
Utkarsh Sharan <utkarsh.sharan99@gmail.com>
2024-12-23 10:43:51 +0530

    updated command invoker

--- Assets/Scripts/Commands/CommandInvoker.cs
+++ Assets/Scripts/Commands/CommandInvoker.cs
@@ -1,4 +1,5 @@
 using Command.AbstractCommand;
+using Command.Main;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -18,6 +19,19 @@ namespace Command
 
         public void ExecuteCommand(ICommand commandToExecute) => commandToExecute.Execute();
 
-        public void RegisterCommand(ICommand commandToRegister) => commandRegistry.Push(commandToRegister); 
+        public void RegisterCommand(ICommand commandToRegister) => commandRegistry.Push(commandToRegister);
+
+        public void Undo()
+        {
+            if (!RegistryEmpty() && CommandBelongsToActivePlayer())     //checking if the registry is empty AND command belongs to active player
+                commandRegistry.Pop().Undo();                           //a player can only undo its own action.
+        }
+
+        private bool RegistryEmpty() => commandRegistry.Count == 0;
+
+        private bool CommandBelongsToActivePlayer()
+        {
+            return (commandRegistry.Peek() as UnitCommand).commandData.ActorPlayerID == GameService.Instance.PlayerService.ActivePlayerID;
+        }
     }
 }
